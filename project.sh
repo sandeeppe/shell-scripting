7fc238c5457856970e2261aeb0577b95a6ffb37c
sandeeppe <sandeekumar.peddi@gmail.com>
2020-08-10 13:03:09 +0530

    mongo coding

--- project.sh
+++ project.sh
@@ -18,22 +18,44 @@ stat(){
 frontend(){
     head "Installing Frontend service"
     yum install nginx -y &>$LOG_FILE
-    stat $? "Nginx Install\t\t"
+    stat $? "\tNginx Install\t\t"
     curl -s -L -o /tmp/frontend.zip "https://dev.azure.com/DevOps-Batches/98e5c57f-66c8-4828-acd6-66158ed6ee33/_apis/git/repositories/65042ce1-fdc2-4472-9aa2-3ae9b87c1ee4/items?path=%2F&versionDescriptor%5BversionOptions%5D=0&versionDescriptor%5BversionType%5D=0&versionDescriptor%5Bversion%5D=master&resolveLfs=true&%24format=zip&api-version=5.0&download=true"
     stat $? "Download Frontend Files" &>>$LOG_FILE
     cd /usr/share/nginx/html
     rm -rf *
     unzip /tmp/frontend.zip &>>$LOG_FILE
-    stat $? "Extract Fontend Files\t"
+    stat $? "\tExtract Fontend Files\t"
     mv static/* .
     rm -rf static README.md
     mv localhost.conf /etc/nginx/nginx.conf
     systemctl enable nginx &>>$LOG_FILE
     systemctl start nginx &>>$LOG_FILE 
-    stat $? "start nginx\t\t"
+    stat $? "\tstart nginx\t\t"
 }
 mongodb(){
     head "Installing Mongodb service"
+    echo '[mongodb-org-4.2]
+    name=MongoDB Repository
+    baseurl=https://repo.mongodb.org/yum/redhat/$releasever/mongodb-org/4.2/x86_64/
+    gpgcheck=1
+    enabled=1
+    gpgkey=https://www.mongodb.org/static/pgp/server-4.2.asc' >/etc/yum.repos.d/mongodb.repo
+    yum install -y mongodb-org 
+    stat $? "Install mongodb server\t"
+    systemctl enable mongodb &>>$LOG_FILE
+    systemctl start mongodb &>>$LOG_FILE
+    stat $? "start MongoDB service\t"
+
+    cd/tmp
+    curl -s -L -o /tmp/mongodb.zip "https://dev.azure.com/DevOps-Batches/98e5c57f-66c8-4828-acd6-66158ed6ee33/_apis/git/repositories/52feee4a-7c54-4f95-b1f5-2051a56b9d76/items?path=%2F&versionDescriptor%5BversionOptions%5D=0&versionDescriptor%5BversionType%5D=0&versionDescriptor%5Bversion%5D=master&resolveLfs=true&%24format=zip&api-version=5.0&download=true" &>>$LOG_FILE
+    stat $? "Download MongoDB Schema\t"
+    unzip -o mongodb.zip &>>$LOG_FILE
+    stat $? "Extracting MongoDB \t"
+    mongo < catalogue.js &>>$LOG_FILE
+    stat $? "Loading Catalogue Schema\t"
+    mongo < user.js &>>$LOG_FILE
+    stat $? "Loading Catalogue Schema\t"
+
 }
 redis(){
     head "Installing Redis service"
