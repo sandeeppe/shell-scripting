20e1571042dab8e5f75cf41a0a09ffa60409dd42
sandeeppe <sandeekumar.peddi@gmail.com>
2020-08-10 19:10:04 +0530

    error correction

--- project.sh
+++ project.sh
@@ -35,11 +35,11 @@ frontend(){
 mongodb(){
     head "Installing Mongodb service"
     echo '[mongodb-org-4.2]
-    name=MongoDB Repository
-    baseurl=https://repo.mongodb.org/yum/redhat/$releasever/mongodb-org/4.2/x86_64/
-    gpgcheck=1
-    enabled=1
-    gpgkey=https://www.mongodb.org/static/pgp/server-4.2.asc' >/etc/yum.repos.d/mongodb.repo
+name=MongoDB Repository
+baseurl=https://repo.mongodb.org/yum/redhat/$releasever/mongodb-org/4.2/x86_64/
+gpgcheck=1
+enabled=1
+gpgkey=https://www.mongodb.org/static/pgp/server-4.2.asc' >/etc/yum.repos.d/mongodb.repo
     yum install -y mongodb-org &>>$LOG_FILE 
     stat $? "Install mongodb server\t"
     systemctl enable mongod &>>$LOG_FILE
