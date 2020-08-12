94af53f58525b7a0e6bc5734dae6486b018acf4b
sandeeppe <sandeekumar.peddi@gmail.com>
2020-08-12 22:34:59 +0530

    updating the rabbitmq function

--- project.sh
+++ project.sh
@@ -112,7 +112,7 @@ MYSQL(){
 }
 RABBITMQ(){
     head "Installing RabbitMQ service"
-    yum list installed | grep esl-erlang >/dev/null
+    yum list installed | grep esl-erlang &>/dev/null
     if [ $? -ne 0 ]; then
       yum install https://packages.erlang-solutions.com/erlang/rpm/centos/7/x86_64/esl-erlang_22.2.1-1~centos~7_amd64.rpm -y &>>$LOG_FILE
       stat $? "Install erland \t"
